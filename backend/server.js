4e3dde3fdbfe3eadb509759c1351ab8d226b98e6
sjadhav <sjadhav@ancestry.com>
2019-06-19 14:45:01 -0700

    Updated url and key

--- backend/server.js
+++ backend/server.js
@@ -22,36 +22,9 @@ app.use(require('body-parser').raw({
 app.post('/activity/execute', (req, res) => {
 	verifyJwt(req.body, Pkg.options.salesforce.marketingCloud.jwtSecret, (err, decoded) => {
 		// verification error -> unauthorized request
-		if (err) {
-			console.error(err);
-			return res.status(401).end();
-		}
 
-		if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
-			let serviceCloudId;
+        return res.status(200).json({branchResult: '<KEY FOR PATH 1>'});
 
-			// TODO: Read the Service Cloud object's Id from inArguments here and
-			// write it to the serviceCloudId variable
-
-			// Call the function that retrieves desired data from Service Cloud
-			sfdc.retrieveFieldOfObject(serviceCloudId, (err, fieldValue) => {
-				if (err) {
-					console.error(err);
-					return res.status(500).end();
-				}
-
-				// Check the returned value to make the decision which path should be
-				// followed and return the branchResult accordingly.
-				if (fieldValue === '<FIELD VALUE THAT LEADS RESULT TO PATH 1>') {
-					return res.status(200).json({branchResult: '<KEY FOR PATH 1>'});
-				} else {
-					return res.status(200).json({branchResult: '<KEY FOR PATH 2>'});
-				}
-			});
-		} else {
-			console.error('inArguments invalid.');
-			return res.status(400).end();
-		}
 	});
 });
 
@@ -61,7 +34,6 @@ app.post(/\/activity\/(save|publish|validate)/, (req, res) => {
 	verifyJwt(req.body, Pkg.options.salesforce.marketingCloud.jwtSecret, (err, decoded) => {
 		// verification error -> unauthorized request
 		if (err)	return res.status(401).end();
-
 		return res.status(200).json({success: true});
 	});
 });
@@ -71,6 +43,6 @@ app.use(express.static(Path.join(__dirname, '..', 'public')));
 
 // Start the server and listen on the port specified by heroku or defaulting to 12345
 app.listen(process.env.PORT || 12345, () => {
-	console.log('Service Cloud customsplit backend is now running!');
+	console.log('Service1 Cloud customsplit backend is now running!');
 });
 
