e66ec83c0b7af4bc99475e88f4d23776289f9f4d
sjadhav <sjadhav@ancestry.com>
2019-07-25 10:46:19 -0700

    Added routes

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,8 @@ app.post('/journeybuilder/seg/execute', async function(req, res){
 
 
 app.post('/journeybuilder/p13n/execute', async function(req, res){
+
+    logData(req);
     console.log('p13n api is called');
 
     count += 1;
@@ -49,6 +51,46 @@ app.post('/journeybuilder/p13n/execute', async function(req, res){
     //console.log('P13n api is called');
     //await  work();
 });
+
+
+function logData(req) {
+    exports.logExecuteData.push({
+                                    body: req.body,
+                                    headers: req.headers,
+                                    trailers: req.trailers,
+                                    method: req.method,
+                                    url: req.url,
+                                    params: req.params,
+                                    query: req.query,
+                                    route: req.route,
+                                    cookies: req.cookies,
+                                    ip: req.ip,
+                                    path: req.path,
+                                    host: req.host,
+                                    fresh: req.fresh,
+                                    stale: req.stale,
+                                    protocol: req.protocol,
+                                    secure: req.secure,
+                                    originalUrl: req.originalUrl
+                                });
+    console.log("body: " + util.inspect(req.body));
+    console.log("headers: " + req.headers);
+    console.log("trailers: " + req.trailers);
+    console.log("method: " + req.method);
+    console.log("url: " + req.url);
+    console.log("params: " + util.inspect(req.params));
+    console.log("query: " + util.inspect(req.query));
+    console.log("route: " + req.route);
+    console.log("cookies: " + req.cookies);
+    console.log("ip: " + req.ip);
+    console.log("path: " + req.path);
+    console.log("host: " + req.host);
+    console.log("fresh: " + req.fresh);
+    console.log("stale: " + req.stale);
+    console.log("protocol: " + req.protocol);
+    console.log("secure: " + req.secure);
+    console.log("originalUrl: " + req.originalUrl);
+}
 
 
 function sleep(ms) {
