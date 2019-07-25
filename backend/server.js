85800f6749159f294c36348d572027f20d8d0980
sjadhav <sjadhav@ancestry.com>
2019-07-25 16:27:53 -0700

    Update content type

--- backend/server.js
+++ backend/server.js
@@ -65,7 +65,7 @@ app.post('/journeybuilder/p13n/execute', function(req, res) {
         }
         res = response;
         console.dir(JSON.parse(body));
-        return res.end();
+        res.status(200).json({success: true});
     })
 });
 
